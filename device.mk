995149a7111510dc17454fdf25174b97e99c1a4f
Sadik Hossain <sadikhossain0854@gmail.com>
2026-09-15 15:43:27 +0000

    a01q: Remove unused ESE-related configurations and services

--- device.mk
+++ device.mk
@@ -277,7 +277,6 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.4.vendor:32 \
     android.hardware.radio.config@1.2.vendor:32 \
     android.hardware.radio.deprecated@1.0.vendor:32 \
-    android.hardware.secure_element@1.0.vendor:32 \
     libxml2 \
     libcutils_shim.vendor:32
 
